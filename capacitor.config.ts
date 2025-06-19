8762f60be4995051a627bc6884c30070c5c2cd3e
Guilherme Tessarini <guilherme.tessarini@hotmail.com>
2025-06-19 02:02:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pokeapi.inonic.angular',
+  appName: 'pokeapi-inonic-angular',
+  webDir: 'www'
+};
+
+export default config;
